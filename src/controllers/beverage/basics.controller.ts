db0960d2d7eff9f7b7f1df7ab8c9c193874f2b28
Tomek Rozalski <github@tomaszrozalski.pl>
2021-09-08 20:06:26 +0200

    [configuration] Finish thinking about routes typings. Start refactoring database, remove all unnecessary relations, for first fire: language relations

--- src/controllers/beverage/basics.controller.ts
+++ src/controllers/beverage/basics.controller.ts
@@ -1,45 +1,93 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
-// import Beverage from '@/models/Beverage/Beverage';
-// import { institution as institutionAggregation } from './aggregations';
+import { isEmpty, unset } from 'lodash';
 
-const getBasics = async (req: FastifyRequest, reply: FastifyReply) => {
-  console.log('props', req.params);
-  // const { language, limit, skip } = req.params;
+import Beverage from '@/models/Beverage/Beverage';
+import { institution as institutionAggregation } from './aggregations';
 
-  // const beverage = await Beverage.aggregate([
-  //   ...institutionAggregation,
-  //   {
-  //     $project: {
-  //       _id: 0,
-  //       id: '$_id',
-  //       shortId: 1,
-  //       badge: 1,
-  //       brand: {
-  //         badge: '$label.general.brand_info.badge',
-  //         name: '$label.general.brand_info.name',
-  //       },
-  //       name: '$label.general.name',
-  //       photos: {
-  //         cover: {
-  //           height: '$editorial.photos.cover.height',
-  //           width: '$editorial.photos.cover.width',
-  //         },
-  //         outlines: {
-  //           cover: '$editorial.photos.outlines.cover',
-  //         },
-  //       },
-  //       container: {
-  //         type: '$label.container.type',
-  //       },
-  //       added: 1,
-  //     },
-  //   },
-  //   { $sort: { added: -1 } },
-  //   // { $skip: +skip },
-  //   // { $limit: +limit },
-  // ]);
+const getBasics = async (
+  req: FastifyRequest<{
+    Params: {
+      language: string;
+      limit: string;
+      skip: string;
+    };
+  }>,
+  reply: FastifyReply,
+) => {
+  const { language, limit, skip } = req.params;
 
-  reply.send({ test: true });
+  console.log({ language });
+
+  const rawBeverage = await Beverage.aggregate([
+    ...institutionAggregation,
+    {
+      $project: {
+        _id: 0,
+        id: '$_id',
+        shortId: 1,
+        badge: 1,
+        brand: {
+          badge: '$label.general.brand_info.badge',
+          name: '$label.general.brand_info.name',
+        },
+        name: '$label.general.name',
+        photos: {
+          cover: {
+            height: '$editorial.photos.cover.height',
+            width: '$editorial.photos.cover.width',
+          },
+          outlines: {
+            cover: '$editorial.photos.outlines.cover',
+          },
+        },
+        container: {
+          type: '$label.container.type',
+        },
+        added: 1,
+      },
+    },
+    { $sort: { added: -1 } },
+    { $skip: +skip },
+    { $limit: +limit },
+  ]);
+
+  const formattedBeverage = rawBeverage.map(({ added, badge, brand, container, id, name, photos, shortId }) => {
+    function translate(a: any) {
+      console.log({ a });
+
+      return { BUM: true };
+    }
+
+    const formatted = {
+      id,
+      shortId,
+      badge,
+      brand: {
+        badge: brand.badge,
+        name: translate(brand.name),
+      },
+      name: translate(name),
+      photos,
+      container,
+      added,
+    };
+
+    if (isEmpty(formatted.photos.cover)) {
+      unset(formatted, 'photos.cover');
+    }
+
+    if (isEmpty(formatted.photos.outlines)) {
+      unset(formatted, 'photos.outlines');
+    }
+
+    if (isEmpty(formatted.photos)) {
+      unset(formatted, 'photos');
+    }
+
+    return formatted;
+  });
+
+  reply.send(formattedBeverage);
 };
 
 export default getBasics;
--- src/routes/beverage/basics.ts
+++ src/routes/beverage/basics.ts
@@ -1,23 +1,8 @@
 import { FastifyPluginAsync } from 'fastify';
-
 import { getBasics } from '@/controllers/beverage';
-import { Basics as BasicsTypes } from '@/utils/types/beverage';
-// import type { QuerystringSchema as QuerystringSchemaTypes } from './types/querystring';
-import QuerystringSchema from '@/utils/schemas/querystring.json';
 
 const basics: FastifyPluginAsync = async (fastify): Promise<void> => {
-  fastify.get<{
-    Body: BasicsTypes[];
-    // Querystring: QuerystringSchemaTypes;
-  }>(
-    '/basics',
-    {
-      schema: {
-        querystring: QuerystringSchema,
-      },
-    },
-    getBasics,
-  );
+  fastify.get('/basics/:language/:skip/:limit', getBasics);
 };
 
 export default basics;
